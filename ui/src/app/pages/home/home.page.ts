1b8ceb2e98fd2eea85aebfecba8dc9fe4a7927bd
GuihMachado <guilhermemachadobr0@gmail.com>
2026-03-20 16:14:52 -0300

    first commit

--- ui/src/app/pages/home/home.page.ts
+++ ui/src/app/pages/home/home.page.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { provideIcons } from '@ng-icons/core';
-import { lucideAlertTriangle, lucideFileUp, lucidePlus, lucideSearch } from '@ng-icons/lucide';
+import { lucideAlbum, lucideAlertTriangle, lucideFileUp, lucideInstagram, lucideMoreVertical, lucidePlus, lucideQrCode, lucideSearch } from '@ng-icons/lucide';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmButtonGroupImports } from '@spartan-ng/helm/button-group';
+import { HlmDropdownMenuImports } from '@spartan-ng/helm/dropdown-menu';
 import { HlmIconImports } from '@spartan-ng/helm/icon';
 import { HlmSeparatorImports } from '@spartan-ng/helm/separator';
 
@@ -12,13 +14,23 @@ import { HlmSeparatorImports } from '@spartan-ng/helm/separator';
         HlmIconImports,
         HlmSeparatorImports,
         HlmButtonImports,
+        HlmDropdownMenuImports,
+        HlmButtonGroupImports
     ],
     providers: [
-        provideIcons({ lucideSearch, lucidePlus, lucideFileUp, lucideAlertTriangle })
+        provideIcons({ lucideSearch, lucidePlus, lucideFileUp, lucideInstagram, lucideMoreVertical, lucideQrCode, lucideAlbum })
     ],
     templateUrl: './home.page.html',
     styleUrl: './home.page.scss'
 })
 export class HomePage {
-    
+    protected redirectTo(to: string) {
+        if (to === 'instagram') {
+            window.open(`https://www.instagram.com/renatamartho/`, '_blank');
+        }else if (to === 'pix') {
+            window.open(`https://www.pix.com/`, '_blank');
+        }else{
+            window.open(`https://loja.marykay.com.br/minha-vitrine?slug=renata-martho-consultora-mary-kay&fbclid=PAY2xjawGx2TNleHRuA2FlbQIxMQABphjH077_ECt3pAZAMdfFA20ZhsrmXZCaLOPAv0uCI8hi-6SqpDsaBIzChw_aem_BgylQnxgkzR25asAM4Ownw`, '_blank');
+        }
+    }
 }

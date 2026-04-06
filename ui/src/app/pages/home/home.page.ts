ded246275159af81754c89577209a4e90f5b5926
Gui Machado <guilhermemachadobr0@gmail.com>
2026-04-06 16:38:02 -0300

    fix: adiciona novos botões para redirecionamento ao WhatsApp e atualiza texto do botão de catálogo

--- ui/src/app/pages/home/home.page.ts
+++ ui/src/app/pages/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { provideIcons } from '@ng-icons/core';
-import { lucideAlbum, lucideAlertTriangle, lucideCheck, lucideCopy, lucideFileUp, lucideInstagram, lucideMoreVertical, lucidePlus, lucideQrCode, lucideSearch, lucideShare2 } from '@ng-icons/lucide';
+import { lucideAlbum, lucideAlertTriangle, lucideCheck, lucideCopy, lucideFileUp, lucideInstagram, lucideMessageCircle, lucideMessageCirclePlus, lucideMoreVertical, lucidePlus, lucideQrCode, lucideSearch, lucideShare2 } from '@ng-icons/lucide';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmButtonGroupImports } from '@spartan-ng/helm/button-group';
 import { HlmDropdownMenuImports } from '@spartan-ng/helm/dropdown-menu';
@@ -22,7 +22,7 @@ import { MatRippleModule } from '@angular/material/core';
         MatRippleModule
     ],
     providers: [
-        provideIcons({ lucideSearch, lucidePlus, lucideFileUp, lucideInstagram, lucideMoreVertical, lucideQrCode, lucideAlbum, lucideCheck, lucideCopy, lucideShare2 })
+        provideIcons({ lucideSearch, lucidePlus, lucideFileUp, lucideInstagram, lucideMoreVertical, lucideQrCode, lucideAlbum, lucideCheck, lucideCopy, lucideShare2, lucideMessageCircle, lucideMessageCirclePlus })
     ],
     templateUrl: './home.page.html',
     styleUrl: './home.page.scss'
@@ -35,6 +35,10 @@ export class HomePage {
             window.open(`https://www.instagram.com/renatamartho/`, '_blank');
         }else if (to === 'pix') {
             window.open(`https://www.pix.com/`, '_blank');
+        }else if (to === 'wpp-team') {
+            window.open(`https://wa.me/5515997177434?text=Olá!+Gostaria+de+fazer+parte+do+seu+time`, '_blank');
+        }else if (to === 'wpp') {
+            window.open(`https://wa.me/5515997177434?text=Olá!`, '_blank');
         }else{
             window.open(`https://loja.marykay.com.br/minha-vitrine?slug=renata-martho-consultora-mary-kay&fbclid=PAY2xjawGx2TNleHRuA2FlbQIxMQABphjH077_ECt3pAZAMdfFA20ZhsrmXZCaLOPAv0uCI8hi-6SqpDsaBIzChw_aem_BgylQnxgkzR25asAM4Ownw`, '_blank');
         }

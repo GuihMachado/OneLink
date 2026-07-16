abf775e1000e1ccec6366c3cf9401a8ef1ad5eb0
GuihMachado <guilhermemachadobr0@gmail.com>
2026-07-15 23:34:51 -0300

    fix: update Supabase URL and anonKey in environment files

--- ui/src/environments/environment.production.ts
+++ ui/src/environments/environment.production.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: true,
     apiUrl: 'https://dev.api.example.com',
     supabase: {
-        url: '',
-        anonKey: '',
+        url: 'https://hjkyaaihmyjkliqlqthy.supabase.co',
+        anonKey: 'sb_publishable_iZOT4iDkWWc1m0w3qUdCDQ_Ot559V2R',
         assetBucket: 'client-assets'
     }
 };
--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:5000',
     supabase: {
-        url: '',
-        anonKey: '',
+        url: 'https://hjkyaaihmyjkliqlqthy.supabase.co',
+        anonKey: 'sb_publishable_iZOT4iDkWWc1m0w3qUdCDQ_Ot559V2R',
         assetBucket: 'client-assets'
     }
 };
